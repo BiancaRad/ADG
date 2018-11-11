388d943eb11c710bbd4aed766ef03bf5d4c9b4d0
BiancaRad <44782444+BiancaRad@users.noreply.github.com>
2018-11-11 23:54:52 +0200

    Add files via upload

---
+++ clasa.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Client {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] vector=new int[6];
+    for(int i=0;i<6;i++){
+       int number = sc.nextInt();
+       vector[i]=number;
+       //System.out.println("Numarul" +i+ "este:"+ number);
+     }
+     for(int i=0;i<6;i++){
+       System.out.print(vector[i]+" ");
+     }
+  }
+}
+#AtelierGoogle
+#ADG
+#AtelierulDigitalJava
